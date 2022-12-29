39df8935c9cac2c4b67be42449d94587339a5abe
MartinEzequielNicolas <martinezequielnicolas@gmail.com>
2022-12-29 18:39:14 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import express from 'express'
 import morgan from 'morgan'
 import {pool} from './db.js'
 import EmployeesRoutes from './routes/employees.routes.js'
-import indexRoutes from './routes/index.routes.js'
 
 const app = express();
 
 app.use(morgan('dev'));
+
 app.use(express.json());
-app.use(indexRoutes);
+
 app.use('/api/',EmployeesRoutes);
+
 app.listen(3000);
+
 console.log('servidor corriendo en el puerto 3000');
